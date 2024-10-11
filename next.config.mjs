d8913a139cb4af67628efa04af392e4d91228b15
JC <jcsacra4@gmail.com>
2024-10-11 22:10:51 +0800

    added next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['https://investment-calculator-jcccc4s-projects.vercel.app/'],
+        domains: ['https://investment-calculator-jcccc4s-projects.vercel.app'],
       },
 };
 
